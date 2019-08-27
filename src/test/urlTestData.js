21cb2150d926b4992af033e3223bbca682c04dfa
tatikaze <k017c1356@it-neec.jp>
2019-08-28 02:24:58 +0900

    [add] init firebase function

---
+++ src/test/urlTestData.js
@@ -0,0 +1,16 @@
+const urlDatas = [
+  {
+    url : 'http://tatikaze.com',
+    title : 'tatikaze.com'
+  },
+  {
+    url : 'https://qiita.com',
+    title : 'qiita.com',
+  },
+  {
+    url : 'https://yahoo.co.jp',
+    title : 'yahoo.co.jp',
+  }
+];
+
+export default urlDatas ;
